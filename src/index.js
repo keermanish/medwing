f8b238665ef7323ed5c0a21afd8730927496ced3
Keer <makeer@deloitte.com>
2019-03-25 00:56:22 +0530

    changes in folder stricture and updated index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import './common.css';
+import Home from './pages/Home/Home';
+import StoreContext from './utils/context';
+import { getStore } from './utils/store';
 import * as serviceWorker from './serviceWorker';
 
+// create app component,
+// wrap it with store context so that
+// app state and dispatch can be accessible in all child cmp
+const App = () => {
+  const {state, dispatch} = getStore();
+
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {/* we can replace with router */}
+      <Home />
+    </StoreContext.Provider>
+  );
+};
+
+// render app
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
